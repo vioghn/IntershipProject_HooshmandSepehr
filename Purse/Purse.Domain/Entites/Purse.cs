8603a3bb1ebe3b0f09955d933b7579629eca76fb
nikimajidifard <nikimajidifard.mfa@gmail.com>
2023-09-03 12:09:32 +0330

    entites creation

---
+++ Purse/Purse.Domain/Entites/Purse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purse.Domain.Entites
+{
+    internal class Purse
+    {
+        public int PurseId { get; set; }
+        public float PurseBalance { get; set; }
+        public int UserID { get; set; }
+
+
+
+
+    }
+}
---
+++ Purse/Purse.Domain/Entites/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purse.Domain.Entites
+{
+    internal class User
+    {
+  
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPhone { get; set; }
+        public string UserRole { get; set; }
+        public int CompanyID { get; set; }
+        public int PurseId { get; set; }
+        public Purse Purse { get; set; }
+     
+            
+        
+    }
+}
