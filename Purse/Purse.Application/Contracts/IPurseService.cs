b0f5d49d5b1331b5f77a86799ffefb6ad07da892
nikimajidifard <nikimajidifard.mfa@gmail.com>
2023-09-03 15:12:55 +0330

    interfaces are added

--- Purse/Purse.Application/Contracts/IPurseService.cs
+++ Purse/Purse.Application/Contracts/IPurseService.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Purse.Domain.Entites;
+
 
 namespace Purse.Application.Contracts
 {
-    internal class IPurseService
+    internal interface IPurseService
     {
+        public string Deposit(float amount);
+        public string Withdraw(float amount);
+        public List<PurseM> GetPurses();
+        public float GetBalance(int PurseId);
+
+
+
+
     }
 }
